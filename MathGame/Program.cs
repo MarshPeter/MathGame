6528a6dee2b0ab7f4f83562971554a7cf959dc05
Peter Farmer <peter.m.farmer.204@gmail.com>
2023-10-17 00:37:49 +1100

    game selection added

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -12,3 +12,20 @@ D - Division
 Q - Quit the program
 ");
 Console.WriteLine("-----------------------------------");
+
+var gameSelected = Console.ReadLine();
+
+if (gameSelected!.Trim().ToLower() == "a") {
+    Console.WriteLine("Addition game Selected");
+} else if (gameSelected!.Trim().ToLower() == "s") {
+    Console.WriteLine("Subtraction game selected");
+} else if (gameSelected!.Trim().ToLower() == "m") {
+    Console.WriteLine("Multiplication game selected");
+} else if (gameSelected!.Trim().ToLower() == "d") {
+    Console.WriteLine("Division game selected");
+} else if (gameSelected!.Trim().ToLower() == "q") {
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+} else {
+    Console.WriteLine("Invalid Input");
+}
