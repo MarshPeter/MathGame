a01fa8f68a263a72dc280c70ef9b988218b16ab5
Peter Farmer <peter.m.farmer.204@gmail.com>
2023-10-17 00:24:41 +1100

    Up to lesson 7 complete

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please type your name");
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("-----------------------------------");
+Console.WriteLine($"Hello {name}. It's {date}. This is your math game.\n");
+Console.WriteLine(@$"What game would you like to play today? Choose options from below: 
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program
+");
+Console.WriteLine("-----------------------------------");
