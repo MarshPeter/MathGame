f512e20bf0088c3a5f70348ac268b92ea9e3c5de
Peter Farmer <peter.m.farmer.204@gmail.com>
2023-10-17 17:05:20 +1100

    refactored project into seperate classes

---
+++ MathGame/Helpers.cs
@@ -0,0 +1,38 @@
+namespace MathGame
+{
+    internal class Helpers
+    {
+        static List<string> games = new();
+        internal static void GetGames() {
+            Console.Clear();
+            Console.WriteLine("Games History");
+            Console.WriteLine("---------------------------------");
+            foreach (var game in games) {
+                Console.WriteLine(game);
+            }
+            Console.WriteLine("---------------------------------\n");
+            Console.WriteLine("Press any button to return to the Main Menu");
+            Console.ReadLine();
+        }
+
+        internal static int[] GetDivisionNumbers() {
+            var random = new Random();
+            var firstNumber = random.Next(1, 99);
+            var secondNumber = random.Next(1, 99);
+            var result = new int[2];
+
+            while (firstNumber % secondNumber != 0) {           
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+            return result;
+        }
+
+        internal static void AddToHistory(int score, string gameType) {
+            games.Add($"{DateTime.Now} - {gameType}: Score = {score}");
+        }
+    }
+}
--- MathGame/Menu.cs
+++ MathGame/Menu.cs
@@ -1,6 +1,50 @@
-
-namespace MathGame {
-  internal class Menu {
-    
+namespace MathGame 
+{
+  internal class Menu
+  {
+    GameEngine gameEngine = new GameEngine();
+    internal void ShowMenu(string? name, DateTime date) {
+        var isGameOn = true;
+        do {
+            Console.Clear();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Hello {name}. It's {date}. This is your math game.\n");
+            Console.WriteLine(@$"What game would you like to play today? Choose options from below: 
+                V - View Previous Games
+                A - Addition
+                S - Subtraction
+                M - Multiplication
+                D - Division
+                Q - Quit the program
+            ");
+            Console.WriteLine("-----------------------------------");
+            var gameSelected = Console.ReadLine();
+            switch (gameSelected!.Trim().ToLower()) {
+                case "a":
+                    gameEngine.AdditionGame("Addition Game");
+                    break; 
+                case "s":
+                    gameEngine.SubtractionGame("Subtraction Game");
+                    break;
+                case "m":
+                    gameEngine.MultiplicationGame("Multiplication Game");
+                    break;
+                case "d":
+                    gameEngine.DivisionGame("Division Game");
+                    break;
+                case "q":
+                    Console.WriteLine("Goodbye");
+                    isGameOn = false;
+                    break;
+                case "v":
+                    Helpers.GetGames();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        } while (isGameOn);
+    }
+      
   }
 }
